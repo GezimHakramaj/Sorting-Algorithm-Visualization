415f50639a6ccef59e158b8d0eacdd9b69948ca4
Gezim Hakramaj <gezimhakramaj@gmail.com>
2020-06-11 19:32:38 -0400

    Initial commit of Bars.js
    Bars.js handles creating/removing the flexBox container which will store the bars represnting the arrays values. Along with creating the bars themselves as well.

---
+++ src/Bars.js
@@ -0,0 +1,24 @@
+function createBars(parent, num, size){ // Method to create a Bar div inside the flexbox container which we dynamically produce and populate in ArrayList's method generateArrayList().
+	var bar = document.createElement("div");
+	bar.className = "box";
+	bar.id = num+1;
+	bar.style.height = ((num+1)/(size*0.01))+"%";
+	parent.appendChild(bar);
+	return bar;
+}
+
+/*
+	Method to create a new flex div which will act as the container to store all other bars in the DOM. This method is invoked whenever we delete the original ArrayList
+	rather than looping through and finding, if size==500, 500 bar divs and deleting those we delete the container dropping all bars.
+*/
+function createFlexBox(){
+	// Drop old flexbox.
+	var oldFlex = document.getElementById("flexContainer");
+	oldFlex.remove();
+	// Create new flexbox.
+	var flex = document.createElement("div");
+	flex.id = "flexContainer"
+	flex.className = "flexbox"
+	document.body.appendChild(flex);
+	return flex;
+}
