1893e00083676fe80c82fec63134745090a100a6
Gezim Hakramaj <gezimhakramaj@gmail.com>
2020-06-11 19:34:23 -0400

    Inital commit of Node.js
    Node.js is an object which stores a value, a bar div and a sorted boolean variable. It is the object we are storing in our ArrayList which will help in keeping our value and our bar div together allowing for easy manipulation of the div elements.

---
+++ src/Node.js
@@ -0,0 +1,25 @@
+class Node{ // Node object will store a value, a reference to the div element, and a sorted value which will be used to change the bar's color.
+
+	constructor(value, bar, sorted){
+		this.value = value;
+		this.bar = bar;
+		this.sorted = sorted;
+	}
+
+	/*
+		When we randomize the array, we will be swapping values to simulate a randomization but not nodes as to retain our referencial div element so we may update the height as below.
+		Since the size of the array will be changing we set the height of the bar to the value of this node divided by the size of the array * 0.01 and then add the percentage sign to 
+		remain syntatically correct. 
+		Ex: Size = 500
+		this.value = 500
+		500/500*0.01 will be 100% which will be the last bar in the visualization being 100% height since it is the largest element. This scales for every size.
+	*/
+	setBarHeight(){
+		this.bar.style.height = ((this.value)/(arr.size()*0.01))+"%";
+	}
+
+
+
+
+
+}
