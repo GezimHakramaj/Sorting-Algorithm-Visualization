ad9ebd7b81a99c187efa0129a70ff41d630e93ee
Gezim Hakramaj <gezimhakramaj@gmail.com>
2020-06-18 20:43:03 -0400

    Updated functions
    Updated sleep function to collaborate with speed buttons used to toggle how fast the algo sorts in real time. Updated insertion, bubble and quick sort removing ugly code using barswap. Added mergesort without visual representation

--- src/Algorithms.js
+++ src/Algorithms.js
@@ -1,23 +1,21 @@
+
+// Helper method to "sleep" for however many miliseconds to allow visualization of sorting rather than
+// an instanteous sort.
+function sleep(ms){
+	console.log(getSpeed()*10);
+	return new Promise(resolve => setTimeout(resolve, ms*getSpeed()*5));
+}
+
 // Insertionsort algorithm
 async function insertionSort(arr){
 	for(var i = 1; i < arr.size(); i++){
 		var j = i-1;
 		var temp = arr[i].value;
 		while(j >= 0 && arr[j].value > temp){
-			arr[j+1].setBarColor("blue");
-			arr[j].setBarColor("blue");
-			await sleep(15);
-			arr.swap(j+1, j);
-			arr[j+1].setBarColor("white");
-			arr[j].setBarColor("white");
+			await arr.barSwap(j+1, j);
 			j--;
 		}
 		arr[j+1].value = temp;
-	}
-
-	for(var i = arr.size()-1; i >= 0; i--){
-		await sleep(25);
-		arr[i].setBarColor("orange");
 	}
 }
 
@@ -25,65 +23,65 @@ async function insertionSort(arr){
 async function bubbleSort(arr){
 	for(var i = 0; i < arr.size(); i++){
 		for(var j = 0; j < arr.size() - 1 - i; j++){
-			if(arr[j].value > arr[j+1].value){
-				arr[j+1].setBarColor("blue");
-				arr[j].setBarColor("blue");
-				await sleep(5);
-				arr.swap(j, j+1);
-				arr[j+1].setBarColor("white");
-				arr[j].setBarColor("white");
-			}
+			if(arr[j].value > arr[j+1].value) await arr.barSwap(j, j+1, offset);
 		}
-		await sleep(20);
+		await sleep();
 		arr[arr.size()-i-1].setBarColor("orange"); // Since bubbleSort, pushes the highest value towards the end, it is always sorted.
 	}
 }
 
 async function quickSort(arr, start, end){
-
 	if(start >= end) return;
+	var index = await partition(arr, start, end);
+	arr[index].setBarColor("white");
 
-	var index = partition(arr, start, end);
-
-	await sleep(75);
-	quickSort(arr, start, index-1);
-	await sleep(75);
-	quickSort(arr, index+1, end);
-
+	await Promise.all([
+		quickSort(arr, start, index-1, ),
+		quickSort(arr, index+1, end, )
+	])
 }
 
-function partition(arr, start, end){
-
+async function partition(arr, start, end){
 	var pivotIndex = start;
 	var pivotValue = arr[end].value;
+	arr[pivotIndex].setBarColor("red");
 	for(var i = start; i < end; i++){
 		if(arr[i].value < pivotValue){
-			arr[i].setBarColor("blue");
-			arr[pivotIndex].setBarColor("blue");
-			arr.swap(i, pivotIndex);
-			arr[i].setBarColor("white");
+			await sleep(1);
 			arr[pivotIndex].setBarColor("white");
+			await arr.barSwap(i, pivotIndex);
 			pivotIndex++;
+			arr[pivotIndex].setBarColor("red");
 		}
 	}
-	arr[pivotIndex].setBarColor("blue");
-	arr[end].setBarColor("blue");
-	arr.swap(pivotIndex, end);
-	arr[pivotIndex].setBarColor("white");
-	arr[end].setBarColor("white");
-	return pivotIndex;
+	await arr.barSwap(pivotIndex, end);
+	return pivotIndex
 }
 
-async function mergeSort(){
 
+async function mergeSort(arr){
+	if(arr.size() < 2) return arr;
+
+	let mid = Math.floor(arr.size()/2);
+	let left = await mergeSort(arr.slice(0, mid));
+	let right = await mergeSort(arr.slice(mid));
+
+	return merge(left, right);
 }
 
-function merge(){
+async function merge(left, right){
 
-}
+	let temp = new ArrayList();
+	while(left.size() && right.size()){
+		if(left[0].value < right[0].value){
+			temp.push(left.shift());
+		}else{
+			temp.push(right.shift());
+		}
+	}
+	temp.concat(left.size()? left : right)
 
-// Helper method to "sleep" for however many miliseconds to allow visualization of sorting rather than
-// an instanteous sort.
-function sleep(milsec){
-	return new Promise(resolve => setTimeout(resolve, milsec));
+
+
+	return temp;
 }
